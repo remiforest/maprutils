ac1b0fb9aa9988168d01f2775f3184b4e3f889d4
remiforest <remi.forest@akilio.com>
2019-04-23 13:59:00 +0200

    added license and setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+import maprutils
+
+setup(name='maprutils',
+      version=maprutils.__version__,
+      packages=find_packages(),
+      description='MapR utilities',
+      url='https://github.com/remiforest/maprutils',
+      author='Remi Forest',
+      author_email='remi.forest@akilio.com',
+      license='LICENSE.txt',
+    )
