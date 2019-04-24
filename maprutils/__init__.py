67f00e86c47aadf84578aa9564f3f606df1ba14b
remiforest <remi.forest@akilio.com>
2019-04-24 20:36:29 +0200

    packaged (again)

---
+++ maprutils/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+# coding: utf-8
+
+"""
+MapR Utilities
+
+Simplifying programming on MapR environments
+
+"""
+
+__version__ = "0.0.1"
+
+import cluster
--- maprutils/cluster.py
+++ maprutils/cluster.py
@@ -1,15 +1,6 @@
 #! /usr/bin/python
 # coding: utf-8
 
-"""
-MapR Utilities
-
-Simplifying programming on MapR environments
-
-"""
-
-__version__ = "0.0.1"
-
 
 """ Cluster utilities """
 
